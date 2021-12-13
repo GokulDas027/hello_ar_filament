ef8bd00bef0973aec98d741c3b9cec9c014b4117
gokuldas <gokuldas027@gmail.com>
2021-12-13 21:42:45 +0530

    Add filament tools plugin to automatically build .filamat materials on project build * Update .mat material files * Update 3d model

---
+++ build-materials.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## matc should either built from source or downloaded for your platform from here:
+## https://github.com/google/filament
+for path in ./app/src/main/materials/* ; do
+  fullname=$(basename "$path")
+  filename="${fullname%.*}"
+  echo "$filename"
+  ./filament/bin/matc -p mobile -a opengl -o "./app/src/main/assets/materials/$filename.filamat" "$path"
+done
---
+++ filament/bin/README.md
@@ -0,0 +1,6 @@
+To build this app, you must first download a copy of
+[Filament](https://github.com/google/filament/releases) for your operating system.
+
+After downloading the release, extract it and copy the binary called `matc` (Linux and macOS) or
+`matc.exe` (Windows) into folder `./filament/bin`. This binary is necessary to compile the shaders
+used by this demo.
